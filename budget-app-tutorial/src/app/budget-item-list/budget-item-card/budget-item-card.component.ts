cedf62a69e8d98999c1a5ff014b8e62b8b01e4a3
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-08-01 11:01:59 +0700

    added expense calculator tutorial project (using angular

---
+++ budget-app-tutorial/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-item-card',
+  templateUrl: './budget-item-card.component.html',
+  styleUrls: ['./budget-item-card.component.scss'],
+})
+export class BudgetItemCardComponent implements OnInit {
+  @Input() isIncome: boolean = true;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
